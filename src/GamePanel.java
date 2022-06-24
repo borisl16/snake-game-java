356d59fd90a1bbc1216a19fd0c50a3ba39d96180
Boris Lazarov <lazarov_borislav@yahoo.com>
2022-06-24 16:56:01 +0300

    adding red apple on screen

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -16,7 +16,7 @@ public class GamePanel extends JPanel implements ActionListener {
     final int x[] = new int[GAME_UNITS];
     final int y[] = new int[UNIT_SIZE];;
     int bodyParts = 6;
-    int applesEaten = 0;
+    int applesEaten;
     int appleX;
     int appleY;
     char direction = 'R';
@@ -25,16 +25,34 @@ public class GamePanel extends JPanel implements ActionListener {
     Random random;
 
     GamePanel() {
-        
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter() );
+        startGame();
     }
     public void startGame() {
-
+        newApple();
+        running = true; 
+        timer = new Timer(DELAY, this);
+        timer.start();
     }
     public void paintComponent(Graphics g) {
-
+        super.paintComponent(g);
+        draw(g);
     }
     public void draw(Graphics g) {
-
+        for (int i = 0; i <SCREEN_HEIGHT/UNIT_SIZE; i++) {
+            g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+        }
+        g.setColor(Color.red);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+    }
+    public void newApple() {
+        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
     }
     public void move() {
 
